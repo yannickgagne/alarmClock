132fa41462ce64bd13eed338dd26831c5bff247c
Yannick Gagne <yannickgagne@users.noreply.github.com>
2015-03-16 15:15:54 -0400

    todo list

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+'''
+TODO:
+- Make a basic GUI
+- Sync NTP at fixed interval
+- Display precise time and date
+- Display next alarm
+- Display a random imgur.com image with alarm
+- 3 screens, current time, config alarm and options, alarm/imgur
+- Choose method to save options
+- Audio alarm, .wav or .mp3, audio amp
+- Touchscreen config
+'''
+
 # import config.py file
 import config as cfg
 
