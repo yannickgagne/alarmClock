ad0f7bd9a03db12601eb946ec4bd7b14fe280f86
Yannick Gagne <yannickg@gmail.com>
2015-03-16 19:48:33 -0400

    suite

--- imgur.py
+++ imgur.py
@@ -1,9 +1,10 @@
 import json, urllib2, random
+import config
 
 imgur = "https://api.imgur.com/3/gallery/random/random/1"
 
 req = urllib2.Request(imgur);
-req.add_header('Authorization', 'Client-ID e894f151f3ef156');
+req.add_header('Authorization', config.client_id);
 res = urllib2.urlopen(req);
 data = json.loads(res.read());
 count = 0;
