c37300e6897be61f0d19012273fde88da9588f01
Yannick Gagne <yannickg@gmail.com>
2015-03-16 19:20:59 -0400

    init

---
+++ gui.py
@@ -0,0 +1,66 @@
+import sys, json, urllib, urllib2, random
+import pygtk
+pygtk.require("2.0")
+import gtk
+import gtk.glade
+	
+class GuiGTK:
+	def __init__(self):
+		self.widgets = gtk.glade.XML('main.glade', 'mainWindow')
+		events = { 'on_button1_clicked': self.monclic,
+					'delete': self.delete }
+		self.widgets.signal_autoconnect(events)
+		
+	def delete(self, source=None, event=None):
+		gtk.main_quit()
+	
+	def monclic(self, source=None, event=None):
+		#self.widgets.get_widget('label1').set_text('test')
+		imgur = "https://api.imgur.com/3/gallery/random/random/1"
+		req = urllib2.Request(imgur);
+		req.add_header('Authorization', 'Client-ID e894f151f3ef156');
+		res = urllib2.urlopen(req);
+		data = json.loads(res.read());
+		count = 0;
+		goodList = [];
+		linky = "";
+		
+		for x in data["data"]:
+			if 'type' in x:
+				if (x["type"] == 'image/jpeg') or (x["type"] == 'image/png'):
+					#print count,x["id"],x["type"],x["link"];
+					goodList.append(count)
+			count += 1;
+		
+		rline = random.choice(goodList)
+		linky = data["data"][rline]["link"]
+		
+		fname = linky.split('/')[-1]
+		fext = fname.split('.')[-1]
+		fnew = 'temp.' + fext
+		urllib.urlretrieve(linky, fnew);
+		self.widgets.get_widget('label1').set_text(fnew)
+		pixbuf = gtk.gdk.pixbuf_new_from_file(fnew);
+
+		maxw = 300
+		maxh = 300
+		orw = pixbuf.get_width()
+		orh = pixbuf.get_height()
+		proph = float(orh)/(float(orw)/maxw)
+		propw = float(orw)/(float(orh)/maxh)
+		
+		if proph > maxh:
+			tarw = int(propw)
+			tarh = int(maxh)
+		else:
+			tarw = int(maxw)
+			tarh = int(proph)
+		
+		pixbuf = pixbuf.scale_simple(tarw, tarh, gtk.gdk.INTERP_BILINEAR);
+				
+		self.widgets.get_widget('image1').set_from_pixbuf(pixbuf);
+		return True
+	
+if __name__ == '__main__':
+	app = GuiGTK()
+	gtk.main()
---
+++ imgur.py
@@ -0,0 +1,23 @@
+import json, urllib2, random
+
+imgur = "https://api.imgur.com/3/gallery/random/random/1"
+
+req = urllib2.Request(imgur);
+req.add_header('Authorization', 'Client-ID e894f151f3ef156');
+res = urllib2.urlopen(req);
+data = json.loads(res.read());
+count = 0;
+goodList = [];
+
+for x in data["data"]:
+	count += 1;
+	if 'type' in x:
+		if (x["type"] == 'image/jpeg') or (x["type"] == 'image/png'):
+			#print count,x["id"],x["type"],x["link"];
+			#print x["type"];
+			goodList.append(count)
+			
+
+print goodList
+print random.choice(goodList)
+
